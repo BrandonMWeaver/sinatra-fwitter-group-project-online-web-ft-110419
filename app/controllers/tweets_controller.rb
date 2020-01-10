106bc419fbcd677dc05d5c1ce41c372c3e0ad394
Brandon Weaver <a7xncob@gmail.com>
2020-01-10 18:05:14 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,7 +26,6 @@ class TweetsController < ApplicationController
   end
   
   get "/tweets/:id" do
-    binding.pry
     if logged_in? && current_user.tweet_ids.include?(params[:id].to_i)
       @tweet = Tweet.find(params[:id])
       erb :"tweets/show"
