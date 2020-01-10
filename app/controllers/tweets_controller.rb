df5ebd66630d7ec38a00169c9a921b92da393ee3
Brandon Weaver <a7xncob@gmail.com>
2020-01-10 17:58:31 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,7 +26,6 @@ class TweetsController < ApplicationController
   end
   
   get "/tweets/:id" do
-    @tweet = Tweet.find(params[:id])
     if logged_in? && current_user.tweets.include?(@tweet)
       erb :"tweets/show"
     end
